f1f5c36f81f65c6d59b901dae33f7c67afb415b8
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-03 17:45:57 +0800

    Update schema script

--- schema/schema.sql
+++ schema/schema.sql
@@ -118,5 +118,21 @@ CREATE TABLE IF NOT EXISTS user_vault.role (
   KEY `role_no` (`role_no`)
 ) ENGINE=InnoDB COMMENT='Roles';
 
+CREATE TABLE IF NOT EXISTS notification (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `notifi_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'notification no',
+  `user_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'user no',
+  `title` varchar(255) NOT NULL DEFAULT '' COMMENT 'title',
+  `message` varchar(1000) NOT NULL DEFAULT '' COMMENT 'message',
+  `status` varchar(10) NOT NULL DEFAULT 'INIT' COMMENT 'Status: INIT, OPENED',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'when the record is created',
+  `created_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who created this record',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'when the record is updated',
+  `updated_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'who updated this record',
+  PRIMARY KEY (`id`),
+  KEY `user_no_status_idx` (`user_no`, `status`),
+  UNIQUE KEY `notifi_no_uk` (`notifi_no`)
+) ENGINE=InnoDB COMMENT='Platform Notification';
+
 -- default one for administrator, with this role, all paths can be accessed
 INSERT INTO user_vault.role(role_no, name) VALUES ('role_554107924873216177918', 'Super Administrator');
